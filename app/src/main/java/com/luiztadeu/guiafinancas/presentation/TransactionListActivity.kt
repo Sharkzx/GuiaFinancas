c4ed87e73accf79caf642f5232d0612a7ff0e983
Luiz Tadeu <luiztadeu@MBP-de-Luiz.home>
2018-06-17 11:55:53 -0300

    include layouts of projects

---
+++ app/src/main/java/com/luiztadeu/guiafinancas/presentation/TransactionListActivity.kt
@@ -0,0 +1,15 @@
+package com.luiztadeu.guiafinancas.presentation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.luiztadeu.guiafinancas.R
+
+class TransactionListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+
+
+    }
+}
